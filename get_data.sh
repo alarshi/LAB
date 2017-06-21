7889165ae9d6f19a080f01bf0987ea9ed49fbade
arushi <al.arushievil@gmail.com>
2017-06-21 16:04:59 -0500

    Modify for openssl.

--- get_data.sh
+++ get_data.sh
@@ -4,5 +4,10 @@ mkdir $now; cd $now/
 wget -P . -m -nd -np -r -A event_* ftp://ftp.iris.washington.edu/pub/userdata/Arushi_Saxena/
 for f in event_*
 do
-  rdseed -df $f 
+  if [ "${f##*.}" == "seed" ]; then
+    rdseed -df $f
+  else 
+    openssl enc -d -des-cbc -salt -in  $f -out "${f%%.*}.seed" -pass pass:Ihn3nLAg
+    rdseed -df "${f%%.*}.seed"
+  fi
 done;
