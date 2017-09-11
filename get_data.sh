602be7645b756f4f2c2628dbab228d8abd1a9452
arushi <al.arushievil@gmail.com>
2017-09-11 14:38:59 -0500

    Modified for deletion of files.

--- get_data.sh
+++ get_data.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-now=$(date +"%m_%d_%Y")
+now=$(date +"%d_%m_%Y")
 mkdir $now; cd $now/
 wget -P . -m -nd -np -r -A event_* ftp://ftp.iris.washington.edu/pub/userdata/Arushi_Saxena/
 for f in event_*
 do
+  mkdir "${f%%.*}";
   if [ "${f##*.}" == "seed" ]; then
+    mv $f "${f%%.*}"/
+    cd "${f%%.*}"/
     rdseed -df $f
+    cd ..
   else 
+    mv $f "${f%%.*}"/
+    cd "${f%%.*}"/
     openssl enc -d -des-cbc -salt -in  $f -out "${f%%.*}.seed" -pass pass:Ihn3nLAg
     rdseed -df "${f%%.*}.seed"
+    cd ..
   fi
 done;
+rm -rf *.seed *.ssl
+
