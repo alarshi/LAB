6bdd06af5f986bc8df0637a4b6e2e80391b894d3
arushi <al.arushievil@gmail.com>
2017-10-31 10:23:00 -0500

    Modified for moving files in history.

--- get_3c.sh
+++ get_3c.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for d in ./event_*
+for d in ./event_00*
 do
   cd $d
   # find the station name which has three seismograms (three components)
-  ls *.SAC | awk -F"." '{print $7,$8}' | sort | uniq -c | awk '{if ($1 % 3 = 0 ) print $2,$3}' | sort | uniq > stn_3c.id
+  ls *.SAC | awk -F"." '{print $7,$8}' | sort | uniq -c | awk '{if ($1 / 3 != 1 ) print $2,$3}' > stn_3c.id
   # change the names of the files 
-  ls `awk '{print "*"$1"."$2".*SAC"}' stn_3c.id` | awk -F"." '{print "mv "$0,$7"."$8"."$10"."$12}' | sh
+  awk '{print "*"$1"."$2".*SAC"}' stn_3c.id | awk '{print "mv " $0 " history"}' | sh 
   cd ..
 done;
