921ade18f5a8e09d55f318fb949e9e5703dde294
arushi saxena <asaxena@memphis.edu>
2019-04-09 16:09:32 -0500

    Update send_request.sh

--- send_request.sh
+++ send_request.sh
@@ -5,7 +5,7 @@ do
   echo '''{"Message":{"Subject": "SeismiQuery request: '"$f"'" ,"Importance": "Normal","Body": {"ContentType": "text","Content": " ''' > post.json
   cat $f >> post.json
   echo '''"},"ToRecipients": [{"EmailAddress": {"Name": "breq_fast@iris.washington.edu","Address": "breq_fast@iris.washington.edu"}}]}} ''' >> post.json
-  curl -X POST --data-binary "@post.json" https://outlook.office365.com/api/v1.0/me/sendmail --header "Content-Type:application/json" --insecure --verbose --user "asaxena@memphis.edu:Ivegotthis03"
+  curl -X POST --data-binary "@post.json" https://outlook.office365.com/api/v1.0/me/sendmail --header "Content-Type:application/json" --insecure --verbose --user "asaxena@memphis.edu:Ivegot******"
  #curl POST -d '{"Message":{"Subject": "SeismiQuery request: '"$f"'" ,"Importance": "Normal","Body": {"ContentType": "text","Content": "'`cat $f`'"},"ToRecipients": [{"EmailAddress": {"Name": "Arushi Saxena","Address": "al.arushievil@gmail.com"}}]}}' https://outlook.office365.com/api/v1.0/me/sendmail --header "Content-Type:application/json" --insecure --verbose --user "asaxena@memphis.edu:Ivegotthis02"
   rm post.json
 done;
